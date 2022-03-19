1e67da744f52f2fe14d033b50b648cd9fb4d7b78
abhimanyusoman123 <101940878+abhimanyusoman123@users.noreply.github.com>
2022-03-19 22:15:28 +0530

    Add files via upload

---
+++ 12_Assignment_5_Create You own built-in class method.py
@@ -0,0 +1,21 @@
+class py_solution:
+   def pow(self, x, n):
+        if x==0 or x==1 or n==1:
+            return x 
+
+        if x==-1:
+            if n%2 ==0:
+                return 1
+            else:
+                return -1
+        if n==0:
+            return 1
+        if n<0:
+            return 1/self.pow(x,-n)
+        val = self.pow(x,n//2)
+        if n%2 ==0:
+            return val*val
+        return val*val*x
+a=int(input("Enter the base Number:"))
+b=int(input("Enter the power Number:"))
+print(py_solution().pow(a, b));
